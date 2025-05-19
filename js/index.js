007691608c101a5b5f4d78b0afffdb8e9c2d0149
Rohit Neupane <63282846+Rohit6094@users.noreply.github.com>
2025-05-19 12:40:15 +0545

    Update index.js

--- js/index.js
+++ js/index.js
@@ -59,7 +59,7 @@ document.getElementById("send_message").addEventListener("click", function () {
   fetch(baserowURL, {
     method: "POST",
     headers: {
-      "Authorization": baserowToken,
+      "Authorization": Token wi5QxZE50rFkHDL42h4SVmNTbq8RZYaj,
       "Content-Type": "application/json"
     },
     body: JSON.stringify(data)
