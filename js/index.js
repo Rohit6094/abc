ddde53dc152e5bc3bd9b15b9a544f7b605e7fe10
Rohit Neupane <neurohit223@gmail.com>
2025-05-19 09:10:54 +0545

    Updated folders

--- js/index.js
+++ js/index.js
@@ -37,3 +37,44 @@ class typeWriter {
 	}
 }
 new typeWriter(text, words, wait);
+
+
+function generateID() {
+    return 'form-' + Math.random().toString(36).substr(2, 9);
+  }
+
+  document.getElementById("send_message").addEventListener("click", function () {
+    const scriptURL = "https://script.google.com/macros/s/AKfycbxV_dXd0GpTuSbOU5opwyV7pXaqOO8jC6ScR3vWTVh87FPxUY-WxR1Ro4GEdye8Fkos/exec"; 
+    const form = document.getElementById("contact_form");
+
+    // Generate and set unique form ID
+    const formId = generateID();
+    document.getElementById("form_id").value = formId;
+
+    // Create JSON object with form data
+    const data = {
+      id: formId,
+      name: form.name.value,
+      email: form.email.value,
+      phone: form.phone.value,
+      message: form.message.value
+    };
+
+    // Send form data to Google Apps Script via POST
+    fetch(scriptURL, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => {
+      alert("Your message has been sent successfully!");
+      form.reset();
+    })
+    .catch(error => {
+      console.error("Error!", error.message);
+      alert("There was an error submitting the form.");
+    });
+  });
+
